b7eee7c8b196419a9915c39703ac8ebdcbd9a019
nooraphussain <noorahussainp2002@gmail.com>
2026-03-07 13:13:56 +0530

    Adding writng file
    The synfunction works first then async then promises

---
+++ project/fileSystem/writeFile.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+
+//Asynchronous
+fs.writeFile('project/fileSystem/original.txt', 'Writing contents to file.', (err, data) => {
+    if(err){
+        console.log('Error while writing file:', err);
+    }else{
+        console.log('File written successfully!')
+    }
+})
+
+
+//Synchronous
+try{
+    let data = fs.writeFileSync('project/fileSystem/original.txt', 'Let\'s write something', 'utf8');
+    console.log('Synchronous writing successful');
+}catch(err){
+    console.error(err);
+}
+
+//using promises;
+await fs.promises.writeFile('project/fileSystem/original.txt', 'Hello async-await');
+
+
+//syn => async => promises (priority order)
